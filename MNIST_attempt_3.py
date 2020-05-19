0d5cd6f8f2d433cc6a9bb6a3baf4c55aaf6f7b90
Hidde <h.fokkema@amsterdamdatacollective.com>
2020-05-19 23:18:26 +0200

    made some figures

--- MNIST_attempt_3.py
+++ MNIST_attempt_3.py
@@ -28,6 +28,7 @@ X, y = fetch_openml('mnist_784', version=1, return_X_y=True)
 X = X[:40000]
 y = y[:40000]
 X_copy = np.copy(X)
+X_copy = X_copy.reshape((40000, 28, 28))
 
 X[X <= .4] = 0
 X[X > .4] = 1
@@ -40,99 +41,105 @@ X_bin = X
 #     img = X[i]
 #     x_arr, y_arr = np.nonzero(img)
 #     X_index[i] = np.array([x_arr, y_arr]).T
-#
-# hf = HeightFiltration()
-# df = DilationFiltration()
-# rf = RadialFiltration(np.array([8, 15]))
-# X_used = X_bin
-# y_used = y
-#
-# ## Create the Filtrations
-# X_hf = hf.fit_transform(X_used, y_used)
-# X_df = df.fit_transform(X_used, y_used)
-# X_rf = rf.fit_transform(X_used, y_used)
-#
-# fig = plt.figure()
-# fig.set_size_inches((12, 9))
-# a = fig.add_subplot(2, 2, 1)
-# a.set_title("Height filtration")
-# imgplot = plt.imshow(X_hf[0], cmap="viridis")
-# a = fig.add_subplot(2,2, 2)
-# a.set_title("Dilation filtration")
-# imgplot = plt.imshow(X_df[0], cmap="viridis")
-# a = fig.add_subplot(2,2, 3)
-# a.set_title("Radial filtration")
-# imgplot = plt.imshow(X_bin[0], cmap="binary")
-# a = fig.add_subplot(2,2, 4)
-# a.set_title("Radial filtration")
-# imgplot = plt.imshow(X_rf[0], cmap="viridis")
-# plt.colorbar(orientation="vertical")
-# plt.savefig("figures/MNINST_example.png")
-#
-#
-# index = np.zeros((28 * 28, 2))
-# k = 0
-# for i in range(28):
-#     for j in range(28):
-#         index[k] = np.array([i, j])
-#         k += 1
-# index = index.astype(int)
-# quality = 10
-# pixels = quality * quality
-# spread = 0.05
-# PI_vectors = np.zeros((40000, 8 * quality * quality))
-# for i in tqdm(range(X_bin.shape[0])):
-#     bin_vec = X_bin[i]
-#     hf_vec = X_hf[i]
-#     df_vec = X_df[i]
-#     rf_vec = X_rf[i]
-#     sampled_index = np.random.choice(len(index), 100, replace=False)
-#     sampled_index = index[sampled_index].astype(int)
-#
-#     sampled_bin_vec = np.concatenate([sampled_index, bin_vec[sampled_index[:, 0], sampled_index[:, 1], None]], axis=1)
-#     sampled_hf_vec = np.concatenate([sampled_index, hf_vec[sampled_index[:, 0], sampled_index[:, 1], None]], axis=1)
-#     sampled_df_vec = np.concatenate([sampled_index, df_vec[sampled_index[:, 0], sampled_index[:, 1], None]], axis=1)
-#     sampled_rf_vec = np.concatenate([sampled_index, rf_vec[sampled_index[:, 0], sampled_index[:, 1], None]], axis=1)
-#     pim = PersImage(spread=spread,
-#                       pixels=[quality, quality],
-#                       kernel_type="gaussian",
-#                       weighting_type="logistic",
-#                       verbose=False,
-#                       specs={
-#                           "maxBD": 2,
-#                           "minBD": 0,
-#                       })
-#
-#     rips = Rips(verbose=False)
-#     dgms_bin = rips.fit_transform(sampled_bin_vec)
-#     dgms_hf = rips.fit_transform(sampled_hf_vec)
-#     dgms_df = rips.fit_transform(sampled_df_vec)
-#     dgms_rf = rips.fit_transform(sampled_rf_vec)
-#
-#     sampled_bin_vec_H0 = pim.transform(dgms_bin[0]).reshape(100)
-#     sampled_bin_vec_H1 = pim.transform(dgms_bin[1]).reshape(100)
-#
-#     sampled_hf_vec_H0 = pim.transform(dgms_hf[0]).reshape(100)
-#     sampled_hf_vec_H1 = pim.transform(dgms_hf[1]).reshape(100)
-#
-#     sampled_df_vec_H0 = pim.transform(dgms_df[0]).reshape(100)
-#     sampled_df_vec_H1 = pim.transform(dgms_df[1]).reshape(100)
-#
-#     sampled_rf_vec_H0 = pim.transform(dgms_rf[0]).reshape(100)
-#     sampled_rf_vec_H1 = pim.transform(dgms_rf[1]).reshape(100)
-#
-#     con = [
-#         sampled_bin_vec_H0,
-#         sampled_bin_vec_H1,
-#         sampled_hf_vec_H0,
-#         sampled_hf_vec_H1,
-#         sampled_df_vec_H0,
-#         sampled_df_vec_H1,
-#         sampled_rf_vec_H0,
-#         sampled_rf_vec_H1,
-#     ]
-#     sampled_con = np.concatenate(con)
-#     PI_vectors[i] = sampled_con
+
+hf = HeightFiltration()
+df = DilationFiltration()
+rf = RadialFiltration(np.array([8, 15]))
+X_used = X_bin
+y_used = y
+
+## Create the Filtrations
+X_hf = hf.fit_transform(X_used, y_used)
+X_df = df.fit_transform(X_used, y_used)
+X_rf = rf.fit_transform(X_used, y_used)
+
+fig = plt.figure()
+fig.set_size_inches((12, 9))
+a = fig.add_subplot(2, 2, 1)
+a.set_title("Height filtration")
+imgplot = plt.imshow(X_hf[4], cmap="viridis")
+a = fig.add_subplot(2,2, 2)
+a.set_title("Dilation filtration")
+imgplot = plt.imshow(X_df[4], cmap="viridis")
+a = fig.add_subplot(2,2, 3)
+a.set_title("Binary")
+imgplot = plt.imshow(X_bin[4], cmap="binary")
+a = fig.add_subplot(2,2, 4)
+a.set_title("Radial filtration")
+imgplot = plt.imshow(X_rf[4], cmap="viridis")
+plt.colorbar(orientation="vertical")
+plt.savefig("figures/MNINST_example.svg", format="svg")
+
+fig = plt.figure()
+fig.set_size_inches((4, 3))
+plt.title("Original picture")
+plt.imshow(X_copy[4], cmap="gray")
+plt.savefig("figures/MNINST_example_original.svg", format="svg")
+
+index = np.zeros((28 * 28, 2))
+k = 0
+for i in range(28):
+    for j in range(28):
+        index[k] = np.array([i, j])
+        k += 1
+index = index.astype(int)
+quality = 20
+pixels = quality * quality
+spread = 0.2
+PI_vectors = np.zeros((5, 8 * quality * quality))
+for i in tqdm([4]):
+    bin_vec = X_bin[i]
+    hf_vec = X_hf[i]
+    df_vec = X_df[i]
+    rf_vec = X_rf[i]
+    sampled_index = np.random.choice(len(index), 100, replace=False)
+    sampled_index = index[sampled_index].astype(int)
+
+    sampled_bin_vec = np.concatenate([sampled_index, bin_vec[sampled_index[:, 0], sampled_index[:, 1], None]], axis=1)
+    sampled_hf_vec = np.concatenate([sampled_index, hf_vec[sampled_index[:, 0], sampled_index[:, 1], None]], axis=1)
+    sampled_df_vec = np.concatenate([sampled_index, df_vec[sampled_index[:, 0], sampled_index[:, 1], None]], axis=1)
+    sampled_rf_vec = np.concatenate([sampled_index, rf_vec[sampled_index[:, 0], sampled_index[:, 1], None]], axis=1)
+    pim = PersImage(spread=spread,
+                      pixels=[quality, quality],
+                      kernel_type="gaussian",
+                      weighting_type="logistic",
+                      verbose=False,
+                      specs={
+                          "maxBD": 2,
+                          "minBD": 0,
+                      })
+
+    rips = Rips(verbose=False)
+    dgms_bin = rips.fit_transform(sampled_bin_vec)
+    dgms_hf = rips.fit_transform(sampled_hf_vec)
+    dgms_df = rips.fit_transform(sampled_df_vec)
+    dgms_rf = rips.fit_transform(sampled_rf_vec)
+
+
+    sampled_bin_vec_H0 = pim.transform(dgms_bin[0]).reshape(100)
+    sampled_bin_vec_H1 = pim.transform(dgms_bin[1]).reshape(100)
+
+    sampled_hf_vec_H0 = pim.transform(dgms_hf[0]).reshape(100)
+    sampled_hf_vec_H1 = pim.transform(dgms_hf[1]).reshape(100)
+
+    sampled_df_vec_H0 = pim.transform(dgms_df[0]).reshape(100)
+    sampled_df_vec_H1 = pim.transform(dgms_df[1]).reshape(100)
+
+    sampled_rf_vec_H0 = pim.transform(dgms_rf[0]).reshape(100)
+    sampled_rf_vec_H1 = pim.transform(dgms_rf[1]).reshape(100)
+
+    con = [
+        sampled_bin_vec_H0,
+        sampled_bin_vec_H1,
+        sampled_hf_vec_H0,
+        sampled_hf_vec_H1,
+        sampled_df_vec_H0,
+        sampled_df_vec_H1,
+        sampled_rf_vec_H0,
+        sampled_rf_vec_H1,
+    ]
+    sampled_con = np.concatenate(con)
+    PI_vectors[i] = sampled_con
 
 # with open("processed_data/PI_data_everything.npy", "wb") as f:
 #     np.save(f, PI_vectors)
---
+++ create_MNIST_figure.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import numpy as np
+from collections import defaultdict
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+plt.style.use('seaborn')
+
+full_pipeline = []
+H1_pipeline = []
+H0_pipeline = []
+con_pipeline = []
+
+full_pipeline.append(pd.read_csv("result_MNIST/MNIST_report_lr.csv")["f1-score"].iloc[10])
+full_pipeline.append(pd.read_csv("result_MNIST/MNIST_report_rf.csv")["f1-score"].iloc[10])
+full_pipeline.append(pd.read_csv("result_MNIST/MNIST_report_sgd.csv")["f1-score"].iloc[10])
+full_pipeline.append(pd.read_csv("result_MNIST/MNIST_report_svm.csv")["f1-score"].iloc[10])
+
+H1_pipeline.append(pd.read_csv("result_MNIST/MNIST_report_H1_lr.csv")["f1-score"].iloc[10])
+H1_pipeline.append(pd.read_csv("result_MNIST/MNIST_report_H1_rf.csv")["f1-score"].iloc[10])
+H1_pipeline.append(pd.read_csv("result_MNIST/MNIST_report_H1_sgd.csv")["f1-score"].iloc[10])
+H1_pipeline.append(pd.read_csv("result_MNIST/MNIST_report_H1_svm.csv")["f1-score"].iloc[10])
+
+H0_pipeline.append(pd.read_csv("result_MNIST/MNIST_report_HO_lr.csv")["f1-score"].iloc[10])
+H0_pipeline.append(pd.read_csv("result_MNIST/MNIST_report_HO_rf.csv")["f1-score"].iloc[10])
+H0_pipeline.append(pd.read_csv("result_MNIST/MNIST_report_HO_sgd.csv")["f1-score"].iloc[10])
+H0_pipeline.append(pd.read_csv("result_MNIST/MNIST_report_HO_svm.csv")["f1-score"].iloc[10])
+
+con_pipeline.append(pd.read_csv("result_MNIST/MNIST_report_con_lr.csv")["f1-score"].iloc[10])
+con_pipeline.append(pd.read_csv("result_MNIST/MNIST_report_con_rf.csv")["f1-score"].iloc[10])
+con_pipeline.append(pd.read_csv("result_MNIST/MNIST_report_con_sgd.csv")["f1-score"].iloc[10])
+con_pipeline.append(pd.read_csv("result_MNIST/MNIST_report_con_svm.csv")["f1-score"].iloc[10])
+
+
+fig, axs = plt.subplots(1,1)
+fig.set_figwidth(24)
+fig.set_figheight(18)
+width = 0.2
+x = np.arange(4)
+clfs = ["Logistic Regression", "Random Forest", "SGD", "SVM"]
+
+axs.bar(x - 3 * width / 2, H0_pipeline, width, label="H0")
+axs.bar(x - width / 2, H1_pipeline, width, label="H1")
+axs.bar(x + width / 2, con_pipeline, width, label="H0 & H1")
+axs.bar(x + 3 * width / 2, full_pipeline, width, label="Custom")
+axs.set_ylabel('Accuracy')
+axs.set_xticks(x)
+axs.set_xticklabels(clfs)
+axs.set_yticks(np.arange(0, 1.1, 0.1))
+axs.legend()
+axs.set_ylim((0, 1))
+axs.set_title("Accuracy of MNIST classification")
+plt.savefig("figures/MNIST_results.svg", format="svg")
